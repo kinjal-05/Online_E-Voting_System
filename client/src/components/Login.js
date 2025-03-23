62bf35c17d13fc1e67d643a1a3a6f2e64216f7d0
Kinjal Mistry <165459555+kinjal-05@users.noreply.github.com>
2025-03-23 14:24:04 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     setError(""); 
 
     try {
-      const response = await axios.post("http://localhost:5000/api/users/login", {
+      const response = await axios.post("https://online-e-voting-system.onrender.com/api/users/login", {
         email,
         password,
       });
