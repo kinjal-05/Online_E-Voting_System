b59c34f3a40c7a5bb809b612ed874836b2196eb7
Kinjal Mistry <165459555+kinjal-05@users.noreply.github.com>
2025-04-26 18:10:19 +0530

    Update AdminDashboard.js

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useAuth } from "../context/AuthContext"; 
 import { useNavigate } from "react-router-dom"; 
-
 import './AdminDashboard.css'; 
 
 const AdminDashboard = () => {
